0750f460eecb49fb22143bd6d20d083026e0d421
Ana Ventura <anaventura1811@gmail.com>
2021-05-25 13:49:23 -0300

    Configura page document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/Head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <h1 className={styles.title}>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      Hello World
+    </h1>
   )
 }
