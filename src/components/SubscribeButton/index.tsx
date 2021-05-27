7eecae4d0f6e97096fc61f8916074db63535b4ca
Ana Ventura <anaventura1811@gmail.com>
2021-05-27 00:18:41 -0300

    feat: checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/client';
 import React from 'react';
 import styles from './styles.module.scss';
 
@@ -6,10 +7,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId } : SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
